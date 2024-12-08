8697207f30374b071cb0f4fb1897bf9730cc9bc7
KinokoLu <myluwork004@gmail.com>
2024-12-08 15:52:31 -0800

    - seperate the data document and the schema rule doc - finished query 1

--- CECS 323 Project 3.py
+++ CECS 323 Project 3.py
@@ -6,8 +6,8 @@ import certifi
 #mongodb://localhost:27017/  <- Hannah's connection string
 # localhost:27017 [kinoko connection]
 try:
-    connection_string = str(input("Input your connection string: "))
-    
+    #connection_string = str(input("Input your connection string: "))
+    connection_string = "localhost:27017"
     # Create MongoDB client
     client = MongoClient(connection_string)
     db = client['project3']
@@ -22,32 +22,40 @@ try:
 
     # FOR TESTING PURPOSES:
     # Print available collections
-    print("Mongo Collection names:")
-    print(db.list_collection_names())
-
-    print("Sample data from artists collection:")
-    for doc in artists.find().limit(5):
-        print(doc)
+    # print("Mongo Collection names:")
+    # print(db.list_collection_names())
+    #
+    # print("Sample data from artists collection:")
+    # for doc in artists.find().limit(5):
+    #     print(doc)
 
 except Exception as e:
     print(f"Error connecting to MongoDB: {e}")
     exit()
 
 while True:
-    print("Select a query number(1-3) or exit(4): \n   1. State report\n   2. Artist search\n3. General admission totals\n   4. Exit")
+    print("Select a query number(1-3) or exit(4): \n   1. State report\n   2. Artist search\n3. General admission totals\n   4. Exit\n")
     try:
-        query_num = int(input())  # Ensure the input is an integer
+        #query_num = int(input("please enter: "))  # Ensure the input is an integer
+        query_num = 1
     except ValueError:
         print("Invalid input. Please enter a number between 1 and 4.")
         continue
 
     if query_num == 1:
         #report = concerts.find({})
-        pass
+        print("--- QUERY 1 ---\n")
+        #myPipeline = venues.aggregate([{'$group': {'_id': '$state','venueCount': {'$sum': 1}}}])
+        #myResult = venues.aggregate(myPipeline)
+
+        for info in (venues.aggregate([{'$group': {'_id': '$state','venueCount': {'$sum': 1}}}])):
+            print(f"State: {info['_id']} | Venues Amount: {info['venueCount']}")
+        break
+
     elif query_num == 2:
-        pass
+        print("query 2")
     elif query_num == 3:
-        pass
+        print("query 3")
     elif query_num == 4:
         print("Exiting program...")
         break
